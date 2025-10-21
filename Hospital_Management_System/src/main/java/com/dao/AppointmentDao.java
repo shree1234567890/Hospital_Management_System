9cc2f7279fe0b5ba32090acd6812e9aa9bab3b50
Prahallad-7 <rriku314@gmail.com>
2025-10-21 10:45:06 +0530

    Add getAppointmentById method to AppointmentDao
    Added method to retrieve appointment by ID.

--- Hospital_Management_System/src/main/java/com/dao/AppointmentDao.java
+++ Hospital_Management_System/src/main/java/com/dao/AppointmentDao.java
@@ -84,6 +84,7 @@ public class AppointmentDao {
 	
 	public List<Appointment> getAllAppointmentByDoctorLogin(int doctorId) {
 	    List<Appointment> list = new ArrayList<>();
+	    Appointment ap = null;
 	    try {
 	        String sql = "select * from appointment where doctor_id=?";
 	        PreparedStatement ps = conn.prepareStatement(sql);
@@ -91,7 +92,7 @@ public class AppointmentDao {
 	        ResultSet rs = ps.executeQuery();
 
 	        while (rs.next()) { 
-	            Appointment ap = new Appointment();
+	            ap = new Appointment();
 	            ap.setId(rs.getInt(1));
 	            ap.setUserId(rs.getInt(2));
 	            ap.setFullName(rs.getString(3));
@@ -113,6 +114,42 @@ public class AppointmentDao {
 	    }
 	    return list;
 	}
+	
+	public Appointment getAppointmentById(int id) {
+		Appointment ap = null;
+	    try {
+	        String sql = "select * from appointment where id=?";
+	        PreparedStatement ps = conn.prepareStatement(sql);
+	        ps.setInt(1, id);
+	        ResultSet rs = ps.executeQuery();
+
+	        while (rs.next()) { 
+	            ap = new Appointment();
+	            ap.setId(rs.getInt(1));
+	            ap.setUserId(rs.getInt(2));
+	            ap.setFullName(rs.getString(3));
+	            ap.setGender(rs.getString(4));
+	            ap.setAge(rs.getString(5));
+	            ap.setAppoinDate(rs.getString(6));
+	            ap.setEmail(rs.getString(7));
+	            ap.setPhNo(rs.getString(8));
+	            ap.setDiseases(rs.getString(9));
+	            ap.setDoctorId(rs.getInt(10));
+	            ap.setAddress(rs.getString(11));
+	            ap.setStatus(rs.getString(12));
+
+	            
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return ap;
+	}
+	
+	
+	
+	
 
 
 
